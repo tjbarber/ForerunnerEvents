a8b1f8821e888d20f869e7b994229d0b6effde5c
TJ Barber <tjbarber@TJs-MacBook-Pro.local>
2017-03-27 20:08:30 -0600

    Initial commit of basic UI and a couple of temp assets

--- TheHaloQuiz/ViewController.swift
+++ TheHaloQuiz/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet var questionLabels: [UILabel]!
+    @IBOutlet var actionButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startGame() {
+        startButton.isHidden = true
+        startButton.isUserInteractionEnabled = false
+        
+        for label in questionLabels {
+            label.isHidden = false
+        }
+        
+        for button in actionButtons {
+            button.isHidden = false
+            button.isUserInteractionEnabled = true
+        }
+    }
 
 }
 
